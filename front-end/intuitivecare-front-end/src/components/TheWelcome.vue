91a179d1c1a5c5a74cbed24180ae024d26fb57a5
iago <iagomauricio7@gmail.com>
2025-03-30 01:03:24 -0300

    front-end funcionando 100%

---
+++ front-end/intuitivecare-front-end/src/components/TheWelcome.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Desenvolvimento</template>
+
+    Este projeto foi desenvolvido por
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">Iago Mauricio</a>
+    e você pode visualizar o código fonte <a href="https://vuejs.org/" target="_blank" rel="noopener">neste
+      repositório.</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Sobre mim</template>
+
+    Me chamo Iago Mauricio, sou estudante de Sistemas de Informação e desenvolvedor fullstack.
+    <a href="https://vite.dev/guide/features.html" target="_blank" rel="noopener">Linkedin</a>.
+    <a href="https://github.com/iagomauricioo" target="_blank" rel="noopener">Github</a>
+
+    <br />
+    <br />
+
+    Leia o README.md para mais informações sobre o projeto.
+  </WelcomeItem>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
