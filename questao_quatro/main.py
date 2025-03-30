10957921bde51b12f8c8d0e03ca25523fb483872
iago <iagomauricio7@gmail.com>
2025-03-30 01:12:45 -0300

    removendo CORS

--- questao_quatro/main.py
+++ questao_quatro/main.py
@@ -4,6 +4,15 @@ import pandas as pd
 import os
 import numpy as np
 from api.v1.routers import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(api_router, prefix="/api/v1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens (desativa o CORS)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
